4b6e353cf949cb612beaa1589a95600ca4a7bd1c
sadrakhxvid777@gmail.com <sadrakhxvid777@gmail.com>
2026-08-20 20:39:27 +0700

    mod: remove second paper clip and modified the first style a bit

--- components/scrapbook/PaperClip.tsx
+++ components/scrapbook/PaperClip.tsx
@@ -8,9 +8,9 @@ export default function PaperClip({ className = "" }: { className?: string }) {
       fill="none"
       aria-hidden="true"
     >
-      <path d="M57 15c-4-12-22-12-26 0l-11 65c-2 14 19 21 25 7l8-51" stroke="#4b3b1c" strokeWidth="3"/>
-      <path d="M58 18c11-8 25 2 21 14l-14 65c-3 13-22 9-19-4l11-54" stroke="#8f773e" strokeWidth="3"/>
-      <path d="M24 81c-3 14 17 25 27 14" stroke="#4b3b1c" strokeWidth="3"/>
+      <path d="M57 15c-4-12-22-12-26 0l-12 65c-2 14 19 21 25 7l10-51" stroke="#4b3b1c" strokeWidth="3"/>
+      {/* <path d="M58 18c11-8 25 2 21 14l-14 65c-3 13-22 9-19-4l11-54" stroke="#8f773e" strokeWidth="3"/> */}
+      {/* <path d="M24 81c-3 14 17 25 27 14" stroke="#4b3b1c" strokeWidth="3"/> */}
     </svg>
   );
 }
