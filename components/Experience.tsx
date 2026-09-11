06c34686b66c2cdcc278151bbfb28980efb80382
sadrakhxvid777@gmail.com <sadrakhxvid777@gmail.com>
2026-09-11 16:08:57 +0700

    mod: changing static link

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -62,9 +62,9 @@ export default function Experience() {
             <h2 className="section-title">GET IN TOUCH</h2>
             <p>Let&apos;s work together.<br />Below is my socials, just hit me up! :)</p>
             <div className="socials">
-              <a className="social" href="https://github.com/" target="_blank" rel="noreferrer">GH</a>
-              <a className="social" href="https://www.linkedin.com/" target="_blank" rel="noreferrer">in</a>
-              <a className="social" href="mailto:sadrakhzp.dev@gmail.com">✉</a>
+              <a className="social" href="https://github.com/sdrzfn" target="_blank" rel="noreferrer">GH</a>
+              <a className="social" href="https://linkedin.com/in/sadrakh-zefanya-putra" target="_blank" rel="noreferrer">in</a>
+              <a className="social" href="mailto:sadrakhxvid777@gmail.com">✉</a>
               <a className="social" href="">↗</a>
             </div>
           </div>
